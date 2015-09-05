3a223df8804364ad3044e71c0b88f13b325a388e
dcunning11235 <dan.p.cunningham@gmail.com>
2015-09-05 11:13:56 -0700

    Tweaks for comparing different predicition methods

--- random_forest_spectra.py
+++ random_forest_spectra.py
@@ -7,6 +7,9 @@ from astropy.table import vstack
 from astropy.table import join
 
 from sklearn import ensemble
+from sklearn import linear_model
+from sklearn import neighbors
+
 from sklearn.decomposition import FastICA
 from sklearn import preprocessing as skpp
 
@@ -62,11 +65,17 @@ def trim_observation_metadata(data, copy=False):
 def main():
     metadata_path = ".."
     spectra_path = "."
+    test_ind = 0
+    #train_span = slice(None,-1,None)
     if len(sys.argv) == 2:
         metadata_path = sys.argv[1]
     if len(sys.argv) == 3:
         metadata_path = sys.argv[1]
         spectra_path = sys.argv[2]
+    if len(sys.argv) == 4:
+        metadata_path = sys.argv[1]
+        spectra_path = sys.argv[2]
+        test_ind = int(sys.argv[3])
 
     obs_metadata = trim_observation_metadata(load_observation_metadata(metadata_path))
     #c_sources, c_mixing, c_exposures, nc_sources, nc_mixing, noncon_exposures = load_all_spectra_data(spectra_path)
@@ -78,36 +87,38 @@ def main():
     sorted_inds = np.argsort(c_exposures)
 
     rfr = ensemble.RandomForestRegressor(n_estimators=150, min_samples_split=1, random_state=rfr_random_state)
+    mtencv = linear_model.MultiTaskElasticNetCV(copy_X=True, normalize=True, n_alphas=200, n_jobs=-1)
+    #random_state=rfr_random_state, selection='random',
+    #rnn = neighbors.RadiusNeighborsRegressor(weights='distance')
+    knn = neighbors.KNeighborsRegressor(weights='distance', n_neighbors=15)
 
     reduced_obs_metadata.remove_column('EXP_ID')
     md_len = len(reduced_obs_metadata)
     X_arr = np.array(reduced_obs_metadata).view('f8').reshape((md_len,-1))
 
-    '''
-    print "--------------------Trainding data-------------------"
-    print X_arr[:-1]
-    print (c_sources[sorted_inds])[-1]
-    print "--------------------Candidate data-------------------"
-    print X_arr[-1]
-    print (c_sources[sorted_inds])[-1]
-    '''
-    test_ind = 0
-    train_span = slice(0,None,None)
+
+    ################################################################
+
+
+    test_X = X_arr[test_ind]
+    train_X = np.vstack( [X_arr[:test_ind], X_arr[test_ind+1:]] )
+    test_y =  (c_sources[sorted_inds])[test_ind]
+    train_y = np.vstack( [(c_sources[sorted_inds])[:test_ind], (c_sources[sorted_inds])[test_ind+1:]] )
 
     print "Which is exposure:", c_exposures[sorted_inds[test_ind]]
 
-    rfr.fit(X=X_arr[train_span], y=(c_sources[sorted_inds])[train_span])
-    #rfr.fit(X=X_arr[:], y=(c_sources[sorted_inds])[:])
-    print rfr.feature_importances_
-
-    prediction = rfr.predict(X_arr[test_ind])
-    print prediction
-
     ica = ICAize.unpickle_FastICA() #(target_type='noncontinuum')
-    predicted_continuum = ica.inverse_transform(prediction, copy=True)
-    print predicted_continuum.shape
-    print c_wavelengths.shape
 
+    rfr.fit(X=train_X, y=train_y)
+    mtencv.fit(X=train_X, y=train_y)
+    #rnn.fit(X=train_X, y=train_y)
+    knn.fit(X=train_X, y=train_y)
+
+    prediction = rfr.predict(test_X)
+    predicted_continuum = ica.inverse_transform(prediction, copy=True)
+
+    data = None
+    actual = None
     plt.plot(c_wavelengths, predicted_continuum[0])
     for file in os.listdir(spectra_path):
         if fnmatch.fnmatch(file, "stacked_sky_*exp{}-continuum.csv".format(c_exposures[sorted_inds[test_ind]])):
@@ -116,11 +127,68 @@ def main():
             data['con_flux'][mask] = np.interp(data['wavelength'][mask], data['wavelength'][~mask], data['con_flux'][~mask])
             actual = data['con_flux']
             #actual = data['flux']
+            delta = predicted_continuum[0] - actual
             plt.plot(c_wavelengths, actual)
-            plt.plot(c_wavelengths, predicted_continuum[0] - actual)
+            plt.plot(c_wavelengths, delta)
+    plt.plot(c_wavelengths, [0]*len(c_wavelengths))
+    err_term = np.sum(np.power(delta, 2))/len(c_wavelengths)
+    plt.legend(['Predicted', 'Actual', 'Delta {:0.5f}'.format(err_term)])
+    plt.tight_layout()
+    plt.title("Random Forest Regressor")
+    plt.show()
+    plt.close()
+
+
+
+    prediction = mtencv.predict(test_X)
+    predicted_continuum = ica.inverse_transform(prediction, copy=True)
+
+    plt.plot(c_wavelengths, predicted_continuum)
+    plt.plot(c_wavelengths, actual)
+    delta = predicted_continuum - actual
+    err_term = np.sum(np.power(delta, 2))/len(c_wavelengths)
+    plt.plot(c_wavelengths, delta)
+    plt.plot(c_wavelengths, [0]*len(c_wavelengths))
+    plt.legend(['Predicted', 'Actual', 'Delta {:0.5f}'.format(err_term)])
+    plt.tight_layout()
+    plt.title("MultiTask Elastic Net CV")
+    plt.show()
+    plt.close()
+
+
+
+    #np.seterr(all='raise')
+    '''
+    prediction = rnn.predict(test_X)
+    print prediction
+
+    predicted_continuum = ica.inverse_transform(prediction, copy=True)
+    print predicted_continuum.shape
+    print c_wavelengths.shape
+
+    plt.plot(c_wavelengths, predicted_continuum[0])
+    plt.plot(c_wavelengths, actual)
+    plt.plot(c_wavelengths, predicted_continuum[0] - actual)
     plt.plot(c_wavelengths, [0]*len(c_wavelengths))
     plt.legend(['Predicted', 'Actual', 'Delta'])
     plt.tight_layout()
+    plt.show()
+    plt.close()
+    '''
+
+
+    prediction = knn.predict(test_X)
+    predicted_continuum = ica.inverse_transform(prediction, copy=True)
+
+    plt.plot(c_wavelengths, predicted_continuum[0])
+    plt.plot(c_wavelengths, actual)
+    delta = predicted_continuum[0] - actual
+    err_term = np.sum(np.power(delta, 2))/len(c_wavelengths)
+    plt.plot(c_wavelengths, delta)
+    plt.plot(c_wavelengths, [0]*len(c_wavelengths))
+    plt.legend(['Predicted', 'Actual', 'Delta {:0.5f}'.format(err_term)])
+    plt.tight_layout()
+    plt.title("Good 'ol K-Nearest")
     plt.show()
     plt.close()
 
