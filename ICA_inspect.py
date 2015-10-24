2161f45d392c8e1c3012ce1bebafd5f3c9cf808d
Daniel Cunningham <dcunningham@darkmatter.ps.uci.edu>
2015-10-23 21:32:56 -0700

    Runtime changes, bug fixes

--- ICA_inspect.py
+++ ICA_inspect.py
@@ -5,7 +5,17 @@ import numpy as np
 
 fastica = ICAize.unpickle_FastICA(target_type="combined")
 for comp_i in range(min(fastica.components_.shape[0], 10)):
-    plt.plot(stack.skyexp_wlen_out, (fastica.components_[comp_i]*1000000)+(5*comp_i) )
+    scale_factor = 2.4/np.max(np.abs(fastica.components_[comp_i]))
+    plt.plot(stack.skyexp_wlen_out, (fastica.components_[comp_i]*scale_factor)+(5*comp_i) )
+
+plt.show()
+plt.close()
+
+
+fastica = ICAize.unpickle_SPCA(target_type="combined")
+for comp_i in range(min(fastica.components_.shape[0], 10)):
+    scale_factor = 2.4/np.max(np.abs(fastica.components_[comp_i]))
+    plt.plot(stack.skyexp_wlen_out, (fastica.components_[comp_i]*scale_factor)+(5*comp_i) )
 
 plt.show()
 plt.close()
--- neg_scanner.py
+++ neg_scanner.py
@@ -18,14 +18,19 @@ def main():
 
     for file in os.listdir(path):
         if fnmatch.fnmatch(file, pattern):
-            data = Table(Table.read(os.path.join(path, file), format="ascii.csv"), masked=True)
+            data = Table.read(os.path.join(path, file), format="ascii.csv")
             mask = data['ivar'] == 0
 
             exp = int(file.split("-")[2][3:9])
 
-            neg_mask = data['flux'][~mask] < -20
-            if np.any(neg_mask):
-                print file, exp, data['wavelength'][~mask][neg_mask][0], data['flux'][~mask][neg_mask][0]
+            #neg_mask = data['flux'][~mask] < -0.5
+            neg_mask = data['flux'] < -0.5
+            set_neg_mask = neg_mask & ~mask
+            if np.any(set_neg_mask):
+                print file, exp, data['wavelength'][set_neg_mask][0], data['flux'][set_neg_mask][0], "repairing..."
+                data['ivar'][set_neg_mask] = 0
+
+                data.write(os.path.join(path, file), format="ascii.csv")
 
 if __name__ == '__main__':
     main()
