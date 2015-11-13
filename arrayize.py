b540b6df2381f47aeb1fe1ffacac3968318aa219
dcunning11235 <dan.p.cunningham@gmail.com>
2015-11-13 06:50:16 -0800

    Added in arrayize

---
+++ arrayize.py
@@ -0,0 +1,12 @@
+import pickle
+import ICAize
+import numpy as np
+
+def main():
+    flux_arr, exp_arr, ivar_arr, mask_arr, wavelengths = \
+        ICAize.load_all_in_dir(spectra_path, use_con_flux=False, recombine_flux=False,
+                        pattern="stacked*exp??????.csv")
+    np.savez("compacted_flux_data.npz", flux=flux_arr, exp=exp_arr, ivar=ivar_arr, wavelengths=wavelengths)
+
+if __name__ == '__main__':
+    main()
